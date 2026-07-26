e14c3b626064fc486db7784bda9783f5b486df6d
Tad <tadesseare@gmail.com>
2026-07-25 22:06:40 -0400

    Prepare frontend for Vercel deployment

--- frontend/src/modules/suppliers/suppliersService.js
+++ frontend/src/modules/suppliers/suppliersService.js
@@ -1,25 +1,16 @@
-import axios from "axios";
+import api from "../../api/api.js";
 
-const API_URL = "http://localhost:5000/api/suppliers";
+const API_URL = "/suppliers";
 
-function authHeader() {
-  const token = localStorage.getItem("token");
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  };
-}
-
-export const getSuppliers = () => axios.get(API_URL, authHeader());
+export const getSuppliers = () => api.get(API_URL);
 
 export const createSupplier = (data) =>
-  axios.post(API_URL, data, authHeader());
+  api.post(API_URL, data);
 
 export const updateSupplier = (id, data) =>
-  axios.put(`${API_URL}/${id}`, data, authHeader());
+  api.put(`${API_URL}/${id}`, data);
 
 export const deleteSupplier = (id) =>
-  axios.delete(`${API_URL}/${id}`, authHeader());
+  api.delete(`${API_URL}/${id}`);
 
 
